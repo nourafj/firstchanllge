68b7e570d453afa484778785e800bba4d144920a
haifa-hub <117362652+haifa-hub@users.noreply.github.com>
2022-11-03 15:54:42 +0300

    Add files via upload

--- ContentView.swift
+++ ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  heelp
+//  contact us
 //
 //  Created by haifa on 02/04/1444 AH.
 //
@@ -9,13 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
+        VStack {
+            VStack{
+                LinearGradient(gradient: Gradient(colors: [Color.orange, Color.purple]), startPoint: .topTrailing, endPoint: .topLeading)
+                    .frame(height: 250.0)
+                    .cornerRadius(25)
+                    .padding(.top, -450.0)
+                ZStack{
+                    
+                    
+                }//vstack
+                .padding(.top, -280.0)
+                
+            }
+            Text("contact us")
+                .font(.title)
+                .padding(.vertical, -270.0)
+            HStack {
+                Image(systemName:"phone.circle")
+            
+                Text("920056412")
+                    .font(.title)
+                    .multilineTextAlignment(.center)
+                    .lineLimit(6)
+            }.padding(.top, -200.0)
+                    .padding(.leading, -160.0)
+                HStack{
+                    Image(systemName:"envelope.circle")
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+                    
+                Text("CusSetaah@mt.gov.sa")
+
+                .foregroundColor(.black)
+                .font(.title)
+                }.padding(.vertical, -100.0)
+                    .padding([.top, .leading], -50.0)
+                    .accentColor(.black)
+            }
+        }
     }
-}
+
+
---
+++ contact_usApp.swift
@@ -0,0 +1,17 @@
+//
+//  contact_usApp.swift
+//  contact us
+//
+//  Created by haifa on 02/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct contact_usApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
