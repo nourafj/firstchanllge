4bcb93dc54fd90797ae05a47087d0acb86ec3a90
noura <85844253+nourafj@users.noreply.github.com>
2022-11-03 15:23:12 +0300

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  teamsPravicy
+//
+//  Created by noura on 08/04/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack{
+        VStack{
+        LinearGradient(gradient: Gradient(colors: [Color.orange, Color.purple]), startPoint: .topTrailing, endPoint: .topLeading)
+        .frame(height: 250.0)
+        .cornerRadius(25)
+        .padding(.top, -170)
+        Text("About Us")
+        .font(.title)
+    
+        .padding(.top, -80.0)
+        }
+    
+Text("Where does it come from? Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia , looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32 The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.")
+           
+                .padding(.all)
+                    }
+                    }
+                 
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ teamsPravicyApp.swift
@@ -0,0 +1,17 @@
+//
+//  teamsPravicyApp.swift
+//  teamsPravicy
+//
+//  Created by noura on 08/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct teamsPravicyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
