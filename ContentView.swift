08fe56c9c18805acec124907b96afa2842efe010
haifa-hub <117362652+haifa-hub@users.noreply.github.com>
2022-11-03 15:53:12 +0300

    Add files via upload

--- ContentView.swift
+++ ContentView.swift
@@ -1,219 +1,75 @@
-//
-//  ContentView.swift
-//  loi
-//
-//  Created by Bashayer  on 05/04/1444 AH.
-//
 
 import SwiftUI
 
 struct ContentView: View {
-    @State private var name: String = "  Name"
-    @State private var mail: String = "  Email"
-    @State private var birth: String = "  MM/DD/YYYY"
-    @State private var pass: String = "  Password"
-    @State private var cpass: String = "  Confirm Password"
-    var body: some View {
-        
-       
+    var body: some View{
+    
         VStack{
-            
-            //color
-    // -------------------------------------------------------------------------------------
-            ZStack{
-                LinearGradient(gradient : Gradient (colors: [Color.orange, Color.purple]), startPoint : .topTrailing, endPoint: .topLeading).cornerRadius(100)
-                                   .frame(height : 250.0)
-                                   
-                                   .padding(.top,-90)
-                                   .padding(.horizontal, -50.0)
-                               Spacer()
-                               
-                               
-                               Text("Sing up")
-                                   .bold()
-                                   .padding(.top, -25.0)
-                               Text("Get started by creating your account")
-                                   .padding(.top, 30.0)
-                                   .font(.caption)
-                
+            VStack{
+            LinearGradient(gradient: Gradient(colors: [Color.orange, Color.purple]), startPoint: .topTrailing, endPoint: .topLeading)
+                .frame(height: 250.0)
+                .cornerRadius(25)
+                .padding(.top, -170)
+            Text("Terms of Service")
+                        .font(.title)
+                        .padding(.top, -80.0)
             }
-            // create fast log in
-    // -------------------------------------------------------------------------------------
-           
-            VStack{
-                
-                Button {
-                             
-                         } label: {
-                             HStack{
-                                 Image("google")
-                                     .resizable()
-                                     .scaledToFit()
-                                     .frame(width: 15,height: 15)
-                                 Text("Continue with Google ")
-                             }
-                             
-                             .frame(width: 250, height: 40)
-                             .foregroundColor(Color.black)
-                             .background(Color.white)
-                             .overlay(
-                                 RoundedRectangle(cornerRadius: 10)
-                                     .stroke(Color.black, lineWidth: 1.5))
-                         }
-                         
-                      
-                         
-                         Button {
-                             
-                         } label: {
-                             HStack{
-                                 Image("f")
-                                     .resizable()
-                                     .scaledToFit()
-                                     .frame(width: 15,height: 15)
-                                 Text("Continue with Facebook")
-                             }
-                             
-                             .frame(width: 250, height: 40)
-                             .foregroundColor(Color.black)
-                             .background(Color.white)
-                             .overlay(
-                                 RoundedRectangle(cornerRadius: 10)
-                                           .stroke(Color.black, lineWidth: 1.5))
-                         }
-                         
-                         
-                         Button {
-                             
-                         } label: {
-                             HStack{
-                                 Image(systemName: "apple.logo")
-                                     .resizable()
-                                     .scaledToFit()
-                                     .frame(width: 15,height: 15)
-                                 Text("Continue with apple")
-                             }
-                             
-                             .frame(width: 250, height: 40)
-                             .foregroundColor(Color.black)
-                             .background(Color.white)
-                             .overlay(
-                                 RoundedRectangle(cornerRadius: 10)
-                                     .stroke(Color.black, lineWidth: 1.5))
-                         }
-                         
-                         
-                         
-                     }
-                     
-
-            .padding()
-            VStack{
-                
-                Divider()
-                    .padding(.leading, 220.0)
-                    .padding(.top , -2)
-                Divider()
-                    .padding(.top , -9)
-                   
-                    .padding(.trailing, 220.0)
-                            
-                Text(" OR ")
-                    .foregroundColor(Color.gray)
-                    .padding(.top , -30)
-                
-                  //  .padding(.top , 20)
-                
-            }.padding()
-                // create  all oftext filed
-    // -------------------------------------------------------------------------------------
-            VStack{
-                        
-                        TextField("Enter your name", text: $name)
-                            .frame(width: 290, height: 40)
-                            .textFieldStyle(.roundedBorder)
-                            .foregroundColor(Color.gray)
-                            .background(Color.white)
-                        
-
-                        
-                        TextField("Enter your Email", text: $mail)
-                            .frame(width: 290, height: 40)
-                            .textFieldStyle(.roundedBorder)
-                            .foregroundColor(Color.gray)
-                            .background(Color.white)
-                       
-
-                        
-                        TextField("Enter your Birtday", text: $birth)
-                            .frame(width: 290, height: 40)
-                            .textFieldStyle(.roundedBorder)
-                            .foregroundColor(Color.gray)
-                            .background(Color.white)
-                       
-                        
-                        TextField("Enter your Password", text: $pass)
-                            .frame(width: 290, height: 40)
-                            .textFieldStyle(.roundedBorder)
-                            .foregroundColor(Color.gray)
-                            .background(Color.white)
-                        
-                        TextField("Confirm your Password", text: $cpass)
-                            .frame(width: 290, height: 40)
-                            .textFieldStyle(.roundedBorder)
-                            .foregroundColor(Color.gray)
-                            .background(Color.white)
-                        
-                        
-                        
-                    }
-                // create congirm bouttn confirm
- // -------------------------------------------------------------------------------------
-            VStack{
-                      
-                       Button("Confirm ") {
-                                          
-                                      }
-                       .frame(width: 100, height: 40)
-                        .foregroundColor(Color.black)
-                           .background(Color.white)
-                           .overlay(
-                               RoundedRectangle(cornerRadius: 10)
-                                   .stroke(Color.purple, lineWidth: 1.5))
-                       
-                       
-                   }
-                // create last text and boutton
-    // -------------------------------------------------------------------------------------
+             Text("Data released while using our services Location data Precise location data. For drivers, cam and audio data, and see which places were displayed when the app was running in startup and on the screen or backgroundis open but not on the screen of their mobile device,Transaction Information: We collect transaction information related to the use of our Services, including the type of services requested or providedUsage data: We collect data about how users interact with our services. This includes data such as the last place you visited, app features or pages viewed, app crashes, other system activity, browser type, and third-party sites or services used before interacting with our services. In some cases, we collect this data through cookies, pixels ")              .padding()
             
-            VStack {
-              
-                Text(" Already have an account?     ")
-                    .padding(.top , 50)
-                    .padding(.trailing , 30)
-                
-            }
             
-            VStack{
-               
-                Button("Sing in ") {
-                                   
-                               }
-                .bold()
-                .foregroundColor(Color.blue)
+            
+            HStack{
+                ZStack{
+                    NavigationLink(destination: Text("Accept"),
+                    label: {
+                    Text("Decline")
+                   // .padding(.leading, -1.0)
+                    .font(.system(size: 20))
+                    .frame(width: 100, height: 40)
+                    .foregroundColor(Color.black)
+                    
                     .background(Color.white)
-                    .padding(.top , -32)
-                    .padding(.leading, 210)
-            }
-            //end of big stack
-    // -------------------------------------------------------------------------------------
+                    .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color.black, lineWidth: 1.5))
+                   // .padding(.leading, 280.0)
+                    } )
+                    }
+                .padding(70)
+                ZStack{
+                    NavigationLink(destination: Text("decline"),
+                    label: {
+                    Text("Accept")
+                           // .padding(.bottom, -20.00),padding(.trailing,  200.00)
+                    //.padding(.leading, -1.0)
+              
+                            .font(.system(size: 20))
+                    .frame(width: 100, height: 40)
+                    .foregroundColor(Color.purple)
+                    .background(Color.white)
+                    .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color.black, lineWidth: 1.5))
+                    //.padding(.leading, 280.0)
+                    } )
+                
+
+                } //.padding(.bottom, -20.00),padding(.trailing,  200.00)//
+                .padding(70)
                 
             }
-
-    }
-}
+            .padding(.all)
+                }
+                }
+                }
+//done
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-    }
-}
+            }
+        }
+
+
+
+    
+  
---
+++ iApp.swift
@@ -0,0 +1,17 @@
+//
+//  iApp.swift
+//  i
+//
+//  Created by haifa on 07/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct iApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
